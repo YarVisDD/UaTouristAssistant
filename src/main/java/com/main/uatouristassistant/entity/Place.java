38415be33fbd17158b4513ce3845e0b67e01ba0b
ZhenyaDikalchuk <y.dykalchuk@gmail.com>
2020-02-20 15:01:32 +0200

    test

--- src/main/java/com/main/uatouristassistant/entity/Place.java
+++ src/main/java/com/main/uatouristassistant/entity/Place.java
@@ -15,7 +15,6 @@ public class Place {
     private String placeDescription;
     @Enumerated(EnumType.STRING)
     private PlaceType placeType;
-
     //TODO: images
     private String imageName;
     //TODO поміняти на юзера
