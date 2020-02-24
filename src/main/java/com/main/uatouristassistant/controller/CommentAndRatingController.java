90060c4a194156cbc1cf4b0591db44c842f7cc8b
YarVisDD <59877101+YarVisDD@users.noreply.github.com>
2020-02-24 19:16:47 +0200

    Add del and update methods

--- src/main/java/com/main/uatouristassistant/controller/CommentAndRatingController.java
+++ src/main/java/com/main/uatouristassistant/controller/CommentAndRatingController.java
@@ -3,7 +3,7 @@ package com.main.uatouristassistant.controller;
 import com.main.uatouristassistant.entity.CommentAndRating;
 import com.main.uatouristassistant.entity.User;
 import com.main.uatouristassistant.repository.CommentAndRatingRepository;
-import com.main.uatouristassistant.repository.UserRepository;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,18 +14,17 @@ import org.springframework.web.bind.annotation.*;
 public class CommentAndRatingController {
     @Autowired
     private CommentAndRatingRepository commentAndRatingRepository;
-    @Autowired
-    private UserRepository userRepository;
+
 
     @PostMapping(path = "/addCommentAndRate")
     public @ResponseBody
-    boolean addCommentAndRate(String comment, String rate) {
+    boolean addCommentAndRate(Long userId,String comment, String rate) {
         CommentAndRating commentAndRating = new CommentAndRating();
-        User user = new User();
 
+        commentAndRating.setUserId(userId);
         commentAndRating.setComment(comment);
         commentAndRating.setRate(rate);
-        commentAndRating.setUserId(user.getUserId());
+
         commentAndRatingRepository.save(commentAndRating);
         return true;
     }
@@ -41,6 +40,33 @@ public class CommentAndRatingController {
     CommentAndRating getCommentAndRate(@RequestParam Long userId) {
 
         return commentAndRatingRepository.findCommentAndRateByUserId(userId);
+    }
+    @PostMapping(path = "/updateCommentAndRate")
+    public @ResponseBody
+    String updateCommentAndRate(@RequestParam Long userId, @RequestParam String comment,String rate) {
+        try {
+            CommentAndRating commentAndRating=commentAndRatingRepository.findCommentAndRateByUserId(userId);
+            commentAndRating.setComment(comment);
+            commentAndRating.setRate(rate);
+            commentAndRatingRepository.save(commentAndRating);
+
+            return "Comment and rate updated";
+        } catch (NullPointerException ex) {
+            return "The user with id " + userId + " does not exist!";
+        }
+    }
+
+    @DeleteMapping(path = "/delCommentAndRate")
+    public @ResponseBody
+    String deleteCommentAndRate(@RequestParam Long userId) {
+        try {CommentAndRating commentAndRating=commentAndRatingRepository.findCommentAndRateByUserId(userId);
+            commentAndRatingRepository.delete(commentAndRating);
+
+
+            return "The comment and rate with userId " + userId + " has been deleted";
+        } catch (Exception ex) {
+            return "The user with userId " + userId + " does not exist!";
+        }
     }
 
 }
--- src/main/java/com/main/uatouristassistant/repository/CommentAndRatingRepository.java
+++ src/main/java/com/main/uatouristassistant/repository/CommentAndRatingRepository.java
@@ -3,7 +3,7 @@ package com.main.uatouristassistant.repository;
 import com.main.uatouristassistant.entity.CommentAndRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentAndRatingRepository extends JpaRepository<CommentAndRating,Integer> {
+public interface CommentAndRatingRepository extends JpaRepository<CommentAndRating,Long> {
     CommentAndRating findCommentAndRateByUserId(Long id);
     CommentAndRating findCommentAndRateByPlaceId(Long id);
 }
