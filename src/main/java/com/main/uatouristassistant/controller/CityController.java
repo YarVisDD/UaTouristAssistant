ad733db915be82914c8f36e3a3e54d265e1835b5
Aleksandr <aleksandrburlutskiy@gmail.com>
2020-03-06 15:42:01 +0200

    Create CityService

--- src/main/java/com/main/uatouristassistant/controller/CityController.java
+++ src/main/java/com/main/uatouristassistant/controller/CityController.java
@@ -1,23 +1,24 @@
 package com.main.uatouristassistant.controller;
 
-import com.main.uatouristassistant.entity.City;
-import com.main.uatouristassistant.repository.CityRepository;
+import com.main.uatouristassistant.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServlet;
 
 @Controller
-@RequestMapping(path = "/city")
-public class CityController {
-    @Autowired
-    private CityRepository cityRepository;
+@RequestMapping("/city")
+public class CityController extends HttpServlet {
 
-    @GetMapping(path = "/listCity")
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping("/listCity")
     @ResponseBody
-    public String getAllAddresses() {
-        List<City> list = cityRepository.findAll();
-        return list.toString();
+    public String AllAddresses() {
+        String list = cityService.getAllAddresses();
+        return "city/listCity";
     }
 }
--- src/main/webapp/show-users.jsp
+++ src/main/webapp/show-users.jsp
@@ -17,8 +17,18 @@
 
 </head>
 <body>
-
-<jsp:include page="header.jsp" />
+<div role="navigation">
+    <div class="navbar navbar-inverse">
+        <a href="/" class="navbar-brand">UA Tourist Assistant</a>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/login">Login</a></li>
+                <li><a href="/registration">New Registration</a></li>
+                <li><a href="/show-users">All Users</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
 
 <div class="container text-center" id="tasksDiv">
     <h3>All Users</h3>
