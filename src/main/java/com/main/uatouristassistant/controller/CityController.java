f251d1f55a339864e62a44b4758b80de481565f2
ZhenyaDikalchuk <y.dykalchuk@gmail.com>
2020-03-06 22:03:40 +0200

    fix city service and controller

--- src/main/java/com/main/uatouristassistant/controller/CityController.java
+++ src/main/java/com/main/uatouristassistant/controller/CityController.java
@@ -1,25 +1,23 @@
 package com.main.uatouristassistant.controller;
 
 import com.main.uatouristassistant.entity.City;
-import com.main.uatouristassistant.repository.CityRepository;
 import com.main.uatouristassistant.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(path = "/city")
 public class CityController {
+
     @Autowired
     private CityService cityService;
 
     @GetMapping(path = "/listCity")
     @ResponseBody
-    public String AllAddress() {
-        List<City> list = cityService.getAllAddress();
-        return "city/listCity";
+    public Iterable<City> allCity() {
+        return cityService.getAllCity();
     }
-
 }
--- src/main/java/com/main/uatouristassistant/service/CityService.java
+++ src/main/java/com/main/uatouristassistant/service/CityService.java
@@ -1,12 +1,10 @@
 package com.main.uatouristassistant.service;
 
-import com.main.uatouristassistant.entity.Address;
 import com.main.uatouristassistant.entity.City;
 import com.main.uatouristassistant.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -15,7 +13,7 @@ public class CityService {
     @Autowired
     private CityRepository cityRepository;
 
-    public List<City> getAllAddress() {
+    public List<City> getAllCity() {
         return cityRepository.findAll();
     }
 }
