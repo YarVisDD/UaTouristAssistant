1f6682f6a50f43c7f0a7f9bc3f15f0e986673527
Aleksandr <aleksandrburlutskiy@gmail.com>
2020-03-09 20:40:48 +0200

    Create CityService addCity and updateCity

--- src/main/java/com/main/uatouristassistant/controller/CityController.java
+++ src/main/java/com/main/uatouristassistant/controller/CityController.java
@@ -4,9 +4,9 @@ import com.main.uatouristassistant.entity.City;
 import com.main.uatouristassistant.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping(path = "/city")
@@ -19,5 +19,31 @@ public class CityController {
     @ResponseBody
     public Iterable<City> AllCity() {
         return cityService.getAllCity();
+    }
+
+
+
+    @PostMapping("/save-city")
+    @ResponseBody
+    public String addCity(@RequestParam String cityName) {
+        cityService.saveCity(cityName);
+        return "redirect:city/show-cities";
+    }
+
+    @RequestMapping("/add-city")
+    public String addCityPage(HttpServletRequest request) {
+        return "city/add-city";
+    }
+
+    @GetMapping("/show-cities")
+    public String showAllCityPage(HttpServletRequest request) {
+        request.setAttribute("city", cityService.getAllCity());
+        return "city/show-cities";
+    }
+
+    @RequestMapping("/update-city/{cityName}")
+    public String updateCity(@PathVariable String cityName, HttpServletRequest request) {
+        request.setAttribute("city", cityService.findCity(cityName));
+        return "/update-city";
     }
 }
--- src/main/java/com/main/uatouristassistant/service/CityService.java
+++ src/main/java/com/main/uatouristassistant/service/CityService.java
@@ -1,9 +1,12 @@
 package com.main.uatouristassistant.service;
 
+import com.main.uatouristassistant.entity.Address;
 import com.main.uatouristassistant.entity.City;
+import com.main.uatouristassistant.entity.User;
 import com.main.uatouristassistant.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,5 +18,24 @@ public class CityService {
 
     public List<City> getAllCity() {
         return cityRepository.findAll();
+    }
+
+
+
+
+    public void saveCity( String cityName) {
+
+        City city;
+        if (cityRepository.findByCityName(cityName) == null) {
+            city = new City();
+            city.setCityName(cityName);
+            cityRepository.save(city);
+        } else {
+            city = cityRepository.findByCityName(cityName);
+        }
+    }
+
+    public City  findCity(String cityName) {
+        return cityRepository.findByCityName(cityName);
     }
 }
