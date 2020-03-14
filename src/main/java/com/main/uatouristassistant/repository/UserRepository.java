5dd1bc713a14a4ede26293f8630caac45c9d548e
Sergii Kogut <skogut@playtika.com>
2020-03-15 00:02:50 +0200

    protect user from hacks

--- src/main/java/com/main/uatouristassistant/repository/UserRepository.java
+++ src/main/java/com/main/uatouristassistant/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByLogin(String login);
+    boolean existsByLogin(String login);
     boolean existsByLoginOrEmail(String login, String Email);
     boolean existsByLoginAndPassword(String login, String password);
 }
